088b5392d23596e45f31e12bd67108bf61143365
VitalGol <vital_gol@ukr.net>
2019-03-21 14:53:35 +0200

    create MongoDB and model

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const keys = require("./keys");
 
 const port = process.env.PORT || 5000;
-const clientPath = path.join(__dirname, 'client');
+const clientPath = path.join(__dirname, "client");
+
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
 const app = express();
 app.use(express.static(clientPath));
 
 app.listen(port, () => {
-	console.log(`server has been started on port ${port}`);
-
+  console.log(`server has been started on port ${port}`);
 });
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://vlad:V!123456@new-blog-rcnbs.mongodb.net/test?retryWrites=true"
+};
