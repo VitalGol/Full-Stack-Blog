971e8db378871a1086f93b496584e9d27de9f16c
VitalGol <vital_gol@ukr.net>
2019-03-21 15:22:25 +0200

    ceated REST API routers

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const postRouter = require("./routes/post");
 const keys = require("./keys");
 
 const port = process.env.PORT || 5000;
 const clientPath = path.join(__dirname, "client");
 
 mongoose
-  .connect(keys.mongoURI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+	.connect(keys.mongoURI)
+	.then(() => console.log("MongoDB connected"))
+	.catch(err => console.error(err));
 
 const app = express();
+app.use("/api/post", postRouter);
 app.use(express.static(clientPath));
 
 app.listen(port, () => {
-  console.log(`server has been started on port ${port}`);
+	console.log(`server has been started on port ${port}`);
 });
